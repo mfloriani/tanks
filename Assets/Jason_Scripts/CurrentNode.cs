b176809c352601e8b159e2f50efb5e5e7efffb73
jturner1998 <mrjturner1998@gmail.com>
2021-03-09 10:53:43 +0000

    Fixed 2D A* Algorithm

--- Assets/Jason_Scripts/CurrentNode.cs
+++ Assets/Jason_Scripts/CurrentNode.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CurrentNode : MonoBehaviour
 {
     [SerializeField] public List<GameObject> accessibleNodes = new List<GameObject>();
+    [SerializeField] public List<GameObject> accessibleNodes2D = new List<GameObject>();
+    [SerializeField] float distance = 2.5f;
 
     Ray ray;
     RaycastHit hit;
+
+    RaycastHit2D[] ray2D;
 
     // Use this for initialization
     void Start()
@@ -15,6 +19,8 @@ public class CurrentNode : MonoBehaviour
 
         //Calls our node function
         CreateNodeList();
+
+        CreateNodeList2D();
     }
 
     private void Update()
@@ -112,6 +118,44 @@ public class CurrentNode : MonoBehaviour
             {
                 accessibleNodes.Add(hit.collider.gameObject);
             }
+        }
+    }
+
+    public void CreateNodeList2D()
+    {
+        accessibleNodes2D.Clear();
+
+        ray2D = Physics2D.RaycastAll(transform.position, transform.TransformDirection(Vector3.left), distance);
+        
+        AddToList2D();
+
+        ray2D = Physics2D.RaycastAll(transform.position, transform.TransformDirection(Vector3.right), distance);
+
+        AddToList2D();
+
+        ray2D = Physics2D.RaycastAll(transform.position, transform.TransformDirection(Vector3.up), distance);
+
+        AddToList2D();
+
+        ray2D = Physics2D.RaycastAll(transform.position, transform.TransformDirection(Vector3.down), distance);
+
+        AddToList2D();
+
+        for(int i = 0; i < accessibleNodes2D.Count; i++)
+        {
+            if(accessibleNodes2D[i] == this.gameObject)
+            {
+                accessibleNodes2D.RemoveAt(i);
+                i = -1;
+            }
+        }
+    }
+
+    void AddToList2D()
+    {
+        for (int i = 0; i < ray2D.Length; i++)
+        {
+            accessibleNodes2D.Add(ray2D[i].collider.gameObject);
         }
     }
 }
