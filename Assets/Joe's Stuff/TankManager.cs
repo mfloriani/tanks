5d381f3ff2aa8db3fdbbd14853831cc91907ba72
Joe <joegreaney98@yahoo.com>
2021-03-15 13:15:26 +0000

    fixed tank rotations being weird in the input manager because the y axes were only taking x axis input

--- Assets/Joe's Stuff/TankManager.cs
+++ Assets/Joe's Stuff/TankManager.cs
@@ -24,7 +24,7 @@ public class TankManager : MonoBehaviour
     private Vector2 _movement;
     [SerializeField]
     private int _player;
-    private float _target;
+
     private GameObject _gun;
     private ParticleSystem _smoke;
 
@@ -90,22 +90,12 @@ public class TankManager : MonoBehaviour
         set { _lTrack = value; }
     }
 
-    public float aimX          //getters and setters for left track speed to be used by other classes
+    public float aim          //getters and setters for target angle to be used by other classes
     {
-        get { return _aimX; }
-        set { _aimX = value; }
-    }
-    public float aimY           //getters and setters for left track speed to be used by other classes
-    {
-        get { return _aimY; }
-        set { _aimY = value; }
+        get { return _aim; }
+        set { _aim = value; }
     }
 
-    public float target           //getters and setters for target angle to be used by other classes
-    {
-        get { return _target; }
-        set { _target = value; }
-    }
 
     public float smoke           //getters and setters for target angle to be used by other classes
     {
@@ -120,14 +110,7 @@ public class TankManager : MonoBehaviour
     }
 
 
-    private float AimAngle()              //convert thumbstick axes data into angle (degrees) for aiming turret
-    {
-        float radAngle = Mathf.Atan2(aimX, aimY);
-        if (radAngle < 0.0f) radAngle += (Mathf.PI * 2.0f);
-        float angle = (180.0f * radAngle / Mathf.PI);
 
-        return angle;
-    }
 
     private void setPlayer()
     {
@@ -152,9 +135,10 @@ public class TankManager : MonoBehaviour
 
     private void Target()
     {
-        target = ((Mathf.Round(AimAngle() / 45) * 45) );
-        target = -target + 180;
-        _gun.transform.eulerAngles = new Vector3(0f, 0f, target);
+
+        aim = ((Mathf.Round(aim / 45) * 45) );
+        aim = -aim + 180;
+        _gun.transform.eulerAngles = new Vector3(0f, 0f, aim);
     }
     
     public void Die()
@@ -204,7 +188,7 @@ public class TankManager : MonoBehaviour
     void FixedUpdate()
     {
         Drive();
-        if(aimX + aimY != 0)
+        if(aim != 0)
         Target();
     }
 
@@ -215,7 +199,7 @@ public class TankManager : MonoBehaviour
 
         if(firing)
         {
-            turret.Fire(target);
+            turret.Fire(aim);
         }
 
         if (honking)
