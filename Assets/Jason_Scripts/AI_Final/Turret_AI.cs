9ee7bf4e15e428dac16fdbbccbb8e30db945ec4e
jturner1998 <mrjturner1998@gmail.com>
2021-04-19 13:46:59 +0100

    AI Rollback
    Rolled Back AI to previous version due to faulty attack mode clashing with rotation this has removed David's request for the AI to look at where it is going before it moves

--- Assets/Jason_Scripts/AI_Final/Turret_AI.cs
+++ Assets/Jason_Scripts/AI_Final/Turret_AI.cs
@@ -6,6 +6,7 @@ public class Turret_AI : MonoBehaviour
 {
     [SerializeField] int tankLayer;
     [SerializeField] int wallLayer;
+    [SerializeField] int dWallLayer;
     [SerializeField] AI_V2 AI;
 
     // Start is called before the first frame update
@@ -13,13 +14,14 @@ public class Turret_AI : MonoBehaviour
     {
         tankLayer = LayerMask.NameToLayer("PlayerTank");
         wallLayer = LayerMask.NameToLayer("Wall");
+        dWallLayer = LayerMask.NameToLayer("DestructibleWall");
         AI = transform.parent.gameObject.GetComponent<AI_V2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       RaycastHit2D ray2D = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.down), Mathf.Infinity, 1 << tankLayer | 1 << wallLayer);
+       RaycastHit2D ray2D = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.down), Mathf.Infinity, 1 << tankLayer | 1 << wallLayer | 1 << dWallLayer);
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.yellow);
 
         if (ray2D.collider != null)
